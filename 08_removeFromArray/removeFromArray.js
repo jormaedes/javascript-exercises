372bd9a1a32ebcb7bc348e97538632a9479ab6cf
Jormaedes Luís <jormahed@gmail.com>
2025-11-09 14:34:24 +0100

    08 is done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(values, ...searcheds) {
+	let indexOfElement;
+	for(const seached of searcheds){
+		indexOfElement = 0;
+		while(indexOfElement != -1){
+			indexOfElement = values.indexOf(seached);
+			if (indexOfElement <= -1) continue;
+			values.splice(indexOfElement, 1);
+		}
+	}
+	return (values);
 };
 
 // Do not edit below this line
