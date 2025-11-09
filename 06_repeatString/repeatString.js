a63fe8860bc375e82a0f6d0e3e72baea34333f2d
Jormaedes Luís <jormahed@gmail.com>
2025-11-09 14:04:54 +0100

    06 is done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+	if (word == '') return '';
+	if (num < 0) return 'ERROR';
+	let result = '';
+	while(num--) result += word;
+	return result;
 };
 
 // Do not edit below this line
