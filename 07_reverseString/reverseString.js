93e8921c4253a8090c3a1ee315df20cea67f2e0c
Jormaedes Luís <jormahed@gmail.com>
2025-11-09 14:12:29 +0100

    07 is done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let result = '';
+	for(let i = string.length-1; i > -1; i--)
+		result += string[i];
+	return (result);
 };
 
 // Do not edit below this line
