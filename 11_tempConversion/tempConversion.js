c1502a50171652aa671ce9ee9f616ee4ab70fce4
Jormaedes Luís <jormahed@gmail.com>
2025-11-09 15:42:18 +0100

    11 is done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const convertToCelsius = function() {
+function safe_return(result){
+	let index = result.toString().indexOf('.');
+	if (index <= -1)
+		return (result);
+	return parseFloat(result.toFixed(1));
+}
+
+const convertToCelsius = function(fahrenheit) {
+	return (safe_return(((fahrenheit - 32) * (5/9))));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+	return safe_return((celsius * (9/5) + 32));
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
