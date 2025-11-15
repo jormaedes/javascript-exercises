5eef190e83512f40f903ae7e4841a27b2c365bf5
Jormaedes Luís <jormahed@gmail.com>
2025-11-15 13:01:46 +0100

    13 is done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+	word = word.toLowerCase();
+	word = word.replaceAll(' ', '').replaceAll('.', '').replaceAll(',', '').replaceAll('!', '');
+	let reverseWord = '';
+	for(let i = word.length-1; i > -1; i--)
+		reverseWord += word[i];
+	return (reverseWord == word);
 };
 
+
+palindromes('ZZZZ car, a man, a maracaz.')
 // Do not edit below this line
 module.exports = palindromes;
